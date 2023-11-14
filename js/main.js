e26d0bb2a9ad9fd515ded08a1b660f286a6bea65
Lawal Warith <79901863+lanre-ll@users.noreply.github.com>
2023-11-14 10:26:24 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,159 @@
+//Show menu
+
+const navMenu = document.getElementById('nav-menu'),
+	navToggle = document.getElementById('nav-toggle'),
+	navClose = document.getElementById('nav-close')
+
+
+//Menu show
+/*Validate if constant exist*/
+if(navToggle){
+	navToggle.addEventListener('click', () =>{
+		navMenu.classList.add('show-menu')
+	})
+}
+
+//Menu Hidden
+/* Validate if constant exists */
+if(navClose){
+	navClose.addEventListener('click', () =>{
+		navMenu.classList.remove('show-menu')
+	})
+}
+
+//Remove menu mobile
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+	const navMenu = document.getElementById('nav-menu')
+	//when we click on each nav__link, we remove tthe show-menu close
+	navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//Shadow Header
+const shadowHeader = () => {
+	const header = document.getElementById('header')
+	//When the scroll is greater than 50 viewport height, add the shadow
+	this.scrollY >= 50 ? header.classList.add('shadow-header')
+					   : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+
+//Email js
+
+const contactForm = document.getElementById('contact-form'),
+	contactMessage = document.getElementById('contact-message')
+
+
+const sendEmail = (e) => {
+	e.preventDefault()
+
+	//serviceID - templateID - #form - publicKey
+	emailjs.sendForm('','','')
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+
+
+/*--------Show scroll up---------*/
+const scrollUp = () => {
+	const scrollUp = document.getElementById('scroll-up')
+	//when the scroll is higher than 350 viewport height, add the scroll
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*------Scroll section active link-------*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+		sectionTop = current.offsetTop - 58,
+		sectionId = current.getAttribute('id'),
+		sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+		if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+			sectionsClass.classList.add('active-link')
+		} else {
+			sectionsClass.classList.remove('active-link')
+		}
+	})
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*------------------Dark Theme-----------------------*/
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+
+//we validated if the user previously chose a topic
+if (selectedTheme) {
+	//if the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+//Activate /deactivate the theme manually with the buttton
+themeButton.addEventListener('click', () => {
+	//Add or remove the dark / icon theme
+	document.body.classList.toggle(darkTheme)
+	themeButton.classList.toggle(iconTheme)
+	// We save the theme and the current icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme())
+	localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+//Scroll reveal
+
+const sr = ScrollReveal ({
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
+	//reset: true // Animations repeat
+})
+
+sr.reveal(`.home__perfil`, {origin: 'right'})
+sr.reveal(`.home__name, .home__info,
+		 .about__container .section__title-1, .about__info, 
+		 .contact__social, .contact__data, .services-list, .testimonials, .result, .brands`, {origin: 'left'})
+sr.reveal(`.process__card, .projects__card`, {interval: 100 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
